c22d32a21800385aedbcfc4aad03eb13c82ee4f0
Wennlys Oliveira <wennlys@gmail.com>
2021-01-01 10:15:49 -0300

    feat: class RemoteAuthentication throws UnexpectedError when HttpPostClients returns 400, 404 or 500

--- src/Data/Usecases/Authentication/RemoteAuthentication.ts
+++ src/Data/Usecases/Authentication/RemoteAuthentication.ts
@@ -1,18 +1,21 @@
 import { HttpPostClient } from '@/Data/Protocols/Http/HttpPostClient';
-import { HttpResponse, HttpStatusCode } from '@/Data/Protocols/Http/HttpResponse';
+import { HttpStatusCode } from '@/Data/Protocols/Http/HttpResponse';
 import { InvalidCredentialsError } from '@/Domain/Errors/InvalidCredentialsError';
+import { UnexpectedError } from '@/Domain/Errors/UnexpectedError';
 import { AuthenticationParams } from '@/Domain/Usecases/Authentication';
 
 export default class RemoteAuthentication {
   constructor(private readonly url: string, private readonly httpPostClient: HttpPostClient) {}
 
-  async auth(params: AuthenticationParams): Promise<HttpResponse> {
+  async auth(params: AuthenticationParams): Promise<void> {
     const response = await this.httpPostClient.post({ url: this.url, body: params });
     switch (response.statusCode) {
+      case HttpStatusCode.ok:
+        break;
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError();
       default:
-        return Promise.resolve(response);
+        throw new UnexpectedError();
     }
   }
 }
