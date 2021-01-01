c9a4c14563fa301c3808ccf43077bf1818847972
Wennlys Oliveira <wennlys@gmail.com>
2021-01-01 11:42:28 -0300

    test: class AxiosHttpClient.post returns correct statusCode & body

--- src/Infra/Http/AxiosHttpClient.test.ts
+++ src/Infra/Http/AxiosHttpClient.test.ts
@@ -1,9 +1,15 @@
 import { AxiosHttpClient } from '@/Infra/Http/AxiosHttpClient';
-import { httpRequestsMock } from '@/Infra/Http/Mocks/httpRequestsMock';
+import { httpPostRequestMock } from '@/Infra/Http/Mocks/httpRequestsMock';
 import axios from 'axios';
+import faker from 'faker';
 
 jest.mock('axios');
 const axiosMock = axios as jest.Mocked<typeof axios>;
+const axiosResponseMock = {
+  data: faker.random.objectElement(),
+  status: faker.random.number(),
+};
+axiosMock.post.mockResolvedValue(axiosResponseMock);
 
 const SUTFactory = (): AxiosHttpClient => {
   return new AxiosHttpClient();
@@ -12,8 +18,17 @@ const SUTFactory = (): AxiosHttpClient => {
 describe('AxiosHttpClient', () => {
   it('calls axios with correct url & body', async () => {
     const SUT = SUTFactory();
-    const request = httpRequestsMock();
+    const request = httpPostRequestMock();
     await SUT.post(request);
     expect(axiosMock.post).toHaveBeenCalledWith(request.url, request.body);
+  });
+
+  it('returns correct statusCode & body', async () => {
+    const SUT = SUTFactory();
+    const response = await SUT.post(httpPostRequestMock());
+    expect(response).toEqual({
+      statusCode: axiosResponseMock.status,
+      body: axiosResponseMock.data,
+    });
   });
 });
--- src/Infra/Http/Mocks/httpRequestsMock.ts
+++ src/Infra/Http/Mocks/httpRequestsMock.ts
@@ -1,7 +1,7 @@
 import { HttpPostClientParams } from '@/Data/Protocols/Http/HttpPostClient';
 import faker from 'faker';
 
-export const httpRequestsMock = (): HttpPostClientParams<unknown> => ({
+export const httpPostRequestMock = (): HttpPostClientParams<unknown> => ({
   url: faker.internet.url(),
   body: faker.random.objectElement(),
 });
