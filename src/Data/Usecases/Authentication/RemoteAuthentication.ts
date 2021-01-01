5e89a7f9b84407cc9750c26dd48afb506a58d4b0
Wennlys Oliveira <wennlys@gmail.com>
2021-01-01 10:32:06 -0300

    feat: class RemoteAuthentication returns an Account when HttpPostclient returns 200

--- src/Data/Usecases/Authentication/RemoteAuthentication.ts
+++ src/Data/Usecases/Authentication/RemoteAuthentication.ts
@@ -3,19 +3,19 @@ import { HttpStatusCode } from '@/Data/Protocols/Http/HttpResponse';
 import { InvalidCredentialsError } from '@/Domain/Errors/InvalidCredentialsError';
 import { UnexpectedError } from '@/Domain/Errors/UnexpectedError';
 import { AccountModel } from '@/Domain/Models/AccountModel';
-import { AuthenticationParams } from '@/Domain/Usecases/Authentication';
+import { Authentication, AuthenticationParams } from '@/Domain/Usecases/Authentication';
 
-export default class RemoteAuthentication {
+export default class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>,
   ) {}
 
-  async auth(params: AuthenticationParams): Promise<void> {
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
     const response = await this.httpPostClient.post({ url: this.url, body: params });
     switch (response.statusCode) {
       case HttpStatusCode.ok:
-        break;
+        return response.body;
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError();
       default:
