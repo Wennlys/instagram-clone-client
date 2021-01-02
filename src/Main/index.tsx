90fda69d989878d6f4212c3f5bea9cff802e0dd5
Wennlys Oliveira <wennlys@gmail.com>
2021-01-02 17:18:43 -0300

    feat: application index moved

---
+++ src/Main/index.tsx
@@ -0,0 +1,5 @@
+import { Profile } from '@/Presentation/Pages/Profile';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Profile />, document.getElementById('App'));
---
+++ src/Presentation/Pages/Login/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Styles from './styles.scss';
+
+export const Login: React.FC = (): JSX.Element => {
+  return <div className={Styles.login}></div>;
+};
