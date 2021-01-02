7ec17916097f7f801034ade2ef5a56ff547a9c52
Wennlys Oliveira <wennlys@gmail.com>
2021-01-02 12:56:16 -0300

    chore: src/main renamed to src/Main

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,13 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/Main/**/*',
     '!<rootDir>/src/**/index.ts',
     '!**/*.d.ts',
   ],
   coverageDirectory: 'coverage',
-  // setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/main/test/cypress'],
+  // setupFilesAfterEnv: ['<rootDir>/src/Main/config/jest-setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/Main/test/cypress'],
   testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest',
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/main/index.tsx',
+  entry: './src/Main/index.tsx',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'main-bundle-[hash].js',
